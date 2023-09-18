6e793973fd838fdd79d5d021887d2a7e445a6d86
william lee <will.lee.npc@gmail.com>
2023-09-17 21:30:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(arg){
+
+  return arg<=400 ? 'This one is on me!'
+    : arg>400 && arg<=2000 ? 'That will be twenty bucks.'
+    : arg>2000 && arg<=2500 ? 'I will gladly take your thirty bucks.'
+    : 'No can do.';
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(arg){
+
+  return arg==="NYC" ? "Ok, sounds good."
+  : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(arg){
+   return arg==='generous' ? 'Thank you so much.'
+   : arg==='not as generous' ? 'Thank you.'
+   :'Bye.';
+   
 }
